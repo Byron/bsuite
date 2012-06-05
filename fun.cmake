15c697490020c20423c414338bfef0fd05bb56d2
Sebastian Thiel <byronimo@gmail.com>
2012-06-05 10:47:51 +0200

    first commit of many more to improve flexibility of the build system. Need to build for mac as well, and support multiple plugins. At some point, the project should be renamed to maya-plugins to allow multiple experiments to be handled nicely

--- fun.cmake
+++ fun.cmake
@@ -9,3 +9,47 @@ function(add_maya_test_executable name testname)
 	get_target_property(execpath ${name} RUNTIME_OUTPUT_DIRECTORY)
 	add_test(${testname} ${execpath}/${name})
 endfunction(add_maya_test_executable)
+
+# This function should be called from a subdirectory denoting the maya version
+# to make sure the include directories are only provided for a single location, and 
+# not for all locations
+# It requires some variables from the central configuration.
+function(add_maya_project VERSION)
+	# set source files
+	set(MAYA_SOURCE_FILES 
+		../maya_main.cpp
+		../visnode.cpp
+		../util.cpp)
+
+	set(_MAYA_LOCATION ${MAYA_INSTALL_BASE_PATH}/maya${VERSION}${MAYA_INSTALL_BASE_SUFFIX})
+	set(_PROJECT Maya${VERSION})
+	
+	if(NOT EXISTS ${_MAYA_LOCATION})
+		message(SEND_ERROR "maya was not found at ${_MAYA_LOCATION} - please assure your MAYA_VERSIONS are set correctly, as well as your MAYA_INSTALL_BASE_SUFFIX, which could be -x64 on 64 bit systems")
+	endif()
+	
+	
+	if(WIN32)
+		set(PROJECT_SUFFIX .mll)
+	elif(APPLE)
+		set(PROJECT_SUFFIX .bundle)
+	else()
+		set(PROJECT_SUFFIX .so)
+	endif()
+	
+	include_directories(${_MAYA_LOCATION}/include)
+	link_directories(${_MAYA_LOCATION}/lib)
+	
+	add_library(${_PROJECT} SHARED ${MAYA_SOURCE_FILES} ${HEADER_FILES})
+	target_link_libraries(${_PROJECT} ${MAYA_LIBRARIES} ${PTEX_LIBRARIES})
+	
+	set_target_properties(${_PROJECT} PROPERTIES 
+											OUTPUT_NAME ptexmaya 
+											SUFFIX ${PROJECT_SUFFIX}
+											CLEAN_DIRECT_OUTPUT 1)
+	
+	if (WIN32)
+		set_target_properties(${_PROJECT} PROPERTIES SUFFIX ".mll")
+	endif()
+endfunction(add_maya_project)
+
--- src/typ.h
+++ src/typ.h
@@ -1,6 +1,6 @@
 #ifndef MAYA_TYPES_H
 #define MAYA_TYPES_H
 
-#include <cstdint>
+#include <cstdint>n
 
 #endif // MAYA_TYPES_H
