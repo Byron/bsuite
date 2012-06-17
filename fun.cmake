3c74eaf7a8363a3fae103a4509ab08d5d85d6a01
Sebastian Thiel <byronimo@gmail.com>
2012-06-17 20:32:13 +0200

    Instead of stopping the loop if one location doesn't exist, it will be continued (unfortunately, there is no continue statement, so the contition is rather convoluted now

--- fun.cmake
+++ fun.cmake
@@ -397,120 +397,121 @@ function(add_maya_project)
 		_maya_project_id(${PROJECT_NAME} ${MAYA_VERSION} PROJECT_ID)
 		
 		if(NOT EXISTS ${_MAYA_LOCATION})
-			message(SEND_ERROR "maya was not found at ${_MAYA_LOCATION} - please assure your MAYA_VERSIONS are set correctly. Otherwise set the ${MAYA_LOCATION_VAR} variable.")
-			return()
-		endif()
-		
-		# MAYA INCLUDE DIR 
-		set(MAYA_INCLUDE_DIR ${_MAYA_LOCATION}/${INCLUDE_SUFFIX})
-		if(NOT EXISTS ${MAYA_INCLUDE_DIR})
-			message(SEND_ERROR "Maya include directory at ${MAYA_INCLUDE_DIR} did not exist")
-			return()
-		endif()
-		
-		# LIBRARY INCLUDE DIR
-		set(MAYA_LIB_DIR ${_MAYA_LOCATION}/${LIB_INSERT})
-		if(NOT EXISTS ${MAYA_LIB_DIR})
-			message(SEND_ERROR "Maya library directory at ${MAYA_LIB_DIR} did not exist")
-			return()
-		endif()
-		
-		set(LATEST_MAYA_INCLUDE_DIR ${MAYA_INCLUDE_DIR})
-		
-		message(STATUS "Setting up ${PROJECT_NAME} for maya ${MAYA_VERSION}")
-		# CREATE TARGET
-		###############
-		add_library(${PROJECT_ID}
-							${PROJECT_TYPE}
-							${PROJECT_SOURCE_FILES})
-		
-		# TARGET LEVEL CONFIGURATION
-		#############################
-		# Sometimes ... we use a different library version than one maya provides (on osx, it may be ptx)
-		# Thus we have to put the project library directories on target level, and BEFORE the maya link directory !
-		foreach(PLIB_DIR IN LISTS PROJECT_LIBRARY_DIRS)
-			append_to_target_property(${PROJECT_ID} LINK_FLAGS "${LINK_DIR_FLAG}\"${PLIB_DIR}\"" YES)
-		endforeach()
-		append_to_target_property(${PROJECT_ID} LINK_FLAGS "${LINK_DIR_FLAG}\"${MAYA_LIB_DIR}\"" YES)
-		if (${CMAKE_BUILD_TYPE} MATCHES Release AND UNIX AND NOT APPLE)
-			append_to_target_property(${PROJECT_ID} LINK_FLAGS "-Wl,--strip-all,-O2" YES)
-		endif()
-		
-		# its ignored by implementation ! Its hard to set per-project includes
-		#pend_to_target_property(${PROJECT_ID} INCLUDE_DIRECTORIES ${MAYA_INCLUDE_DIR})
-		#set_property(TARGET ${PROJECT_ID}
-		#			APPEND PROPERTY INCLUDE_DIRECTORIES ${MAYA_INCLUDE_DIR})
-		append_to_target_property(${PROJECT_ID} COMPILE_FLAGS "${INCL_DIR_FLAG}\"${MAYA_INCLUDE_DIR}\"" YES)
-		if(PROJECT_WITHOUT_EXCEPTIONS AND UNIX)
-			# TODO: windows version ... 
-			append_to_target_property(${PROJECT_ID} COMPILE_FLAGS -fno-exceptions YES)
-		endif()
-		
-		if (PROJECT_WITH_OPENMP AND OPENMP_FOUND)
-			append_to_target_property(${PROJECT_ID} COMPILE_FLAGS "${OpenMP_CXX_FLAGS}" YES)
-			
-			# for now, on osx, we have to link the omp library with it. Lets try to 
-			if(APPLE)
-				append_to_target_property(${PROJECT_ID} LINK_FLAGS "-lgomp" YES)
-			endif()
-		endif()
-		
-		# Make sure we get the osx 10.6 libs
-		if(APPLE)
-			append_to_target_property(${PROJECT_ID} LINK_FLAGS "-isysroot \"${CMAKE_FRAMEWORK_PATH}\"" YES)
-		endif()
-		
-		target_link_libraries(${PROJECT_ID} 
-										${DEFAULT_MAYA_LIBRARIES} 
-										${PROJECT_LINK_LIBRARIES}
-										${OS_SPECIFIC_LIBRARIES})
-		
-		# append maya specic libraries
-		foreach(LIBRARY_NAME IN LISTS PROJECT_LINK_MAYA_LIBRARIES)
-			_maya_project_id(${LIBRARY_NAME} ${MAYA_VERSION} LIBRARY_ID)
-			target_link_libraries(${PROJECT_ID} 
-									${LIBRARY_ID})
-		endforeach()
-		
-		if(PROJECT_DEFINES)
-			append_to_target_property(${PROJECT_ID} COMPILE_DEFINITIONS "${PROJECT_DEFINES}" NO)
-		endif()
-		
-		if (IS_MODULE)
-			set_target_properties(${PROJECT_ID} PROPERTIES
-													OUTPUT_NAME 
-															${PROJECT_NAME}
-													PREFIX
-															""				# make sure we don't get the 'lib' prefix on linux
-													SUFFIX
-															${PROJECT_SUFFIX}
-													LIBRARY_OUTPUT_DIRECTORY
-														${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/${MAYA_VERSION}
-													CLEAN_DIRECT_OUTPUT 1)
-			
-			if(PROJECT_WITH_TEST)
-				# Assure configuration
-				if(EXISTS ${TEST_TMRV_PATH})
-					add_test(NAME
-								${PROJECT_ID}
-							WORKING_DIRECTORY
-								${CMAKE_SOURCE_DIR}/test
-							COMMAND 
-								python ${TEST_TMRV_PATH} ${MAYA_VERSION} 
-										--mrv-mayapy batch_startup.py 
-										$<TARGET_FILE:${PROJECT_ID}>)
-					message(STATUS "Adding test for ${PROJECT_NAME} for maya ${MAYA_VERSION}")
-				else()
-					message(STATUS "Tests for ${PROJECT_ID} deactivated as tmrv path is not provided, check your configuration")
-				endif() # project with tmrv path
-			endif() # project with test
+			message(WARNING "maya was not found at ${_MAYA_LOCATION} - please assure your MAYA_VERSIONS are set correctly. Otherwise set the ${MAYA_LOCATION_VAR} variable.")
 		else()
-			set_target_properties(${PROJECT_ID} PROPERTIES
-													OUTPUT_NAME 
-															${PROJECT_NAME}
-													ARCHIVE_OUTPUT_DIRECTORY
-														${CMAKE_ARCHIVE_OUTPUT_DIRECTORY}/${MAYA_VERSION})
-		endif()# end handle module/static
+			# MAYA INCLUDE DIR 
+			set(MAYA_INCLUDE_DIR ${_MAYA_LOCATION}/${INCLUDE_SUFFIX})
+			if(NOT EXISTS ${MAYA_INCLUDE_DIR})
+				message(SEND_ERROR "Maya include directory at ${MAYA_INCLUDE_DIR} did not exist")
+				return()
+			endif()
+			
+			# LIBRARY INCLUDE DIR
+			set(MAYA_LIB_DIR ${_MAYA_LOCATION}/${LIB_INSERT})
+			if(NOT EXISTS ${MAYA_LIB_DIR})
+				message(SEND_ERROR "Maya library directory at ${MAYA_LIB_DIR} did not exist")
+				return()
+			endif()
+			
+			set(LATEST_MAYA_INCLUDE_DIR ${MAYA_INCLUDE_DIR})
+			
+			message(STATUS "Setting up ${PROJECT_NAME} for maya ${MAYA_VERSION}")
+			# CREATE TARGET
+			###############
+			add_library(${PROJECT_ID}
+								${PROJECT_TYPE}
+								${PROJECT_SOURCE_FILES})
+			
+			# TARGET LEVEL CONFIGURATION
+			#############################
+			# Sometimes ... we use a different library version than one maya provides (on osx, it may be ptx)
+			# Thus we have to put the project library directories on target level, and BEFORE the maya link directory !
+			foreach(PLIB_DIR IN LISTS PROJECT_LIBRARY_DIRS)
+				append_to_target_property(${PROJECT_ID} LINK_FLAGS "${LINK_DIR_FLAG}\"${PLIB_DIR}\"" YES)
+			endforeach()
+			append_to_target_property(${PROJECT_ID} LINK_FLAGS "${LINK_DIR_FLAG}\"${MAYA_LIB_DIR}\"" YES)
+			if (${CMAKE_BUILD_TYPE} MATCHES Release AND UNIX AND NOT APPLE)
+				append_to_target_property(${PROJECT_ID} LINK_FLAGS "-Wl,--strip-all,-O2" YES)
+			endif()
+			
+			# its ignored by implementation ! Its hard to set per-project includes
+			#pend_to_target_property(${PROJECT_ID} INCLUDE_DIRECTORIES ${MAYA_INCLUDE_DIR})
+			#set_property(TARGET ${PROJECT_ID}
+			#			APPEND PROPERTY INCLUDE_DIRECTORIES ${MAYA_INCLUDE_DIR})
+			append_to_target_property(${PROJECT_ID} COMPILE_FLAGS "${INCL_DIR_FLAG}\"${MAYA_INCLUDE_DIR}\"" YES)
+			if(PROJECT_WITHOUT_EXCEPTIONS AND UNIX)
+				# TODO: windows version ... 
+				append_to_target_property(${PROJECT_ID} COMPILE_FLAGS -fno-exceptions YES)
+			endif()
+			
+			if (PROJECT_WITH_OPENMP AND OPENMP_FOUND)
+				append_to_target_property(${PROJECT_ID} COMPILE_FLAGS "${OpenMP_CXX_FLAGS}" YES)
+				
+				# for now, on osx, we have to link the omp library with it. Lets try to 
+				if(APPLE)
+					append_to_target_property(${PROJECT_ID} LINK_FLAGS "-lgomp" YES)
+				endif()
+			endif()
+			
+			# Make sure we get the osx 10.6 libs
+			if(APPLE)
+				append_to_target_property(${PROJECT_ID} LINK_FLAGS "-isysroot \"${CMAKE_FRAMEWORK_PATH}\"" YES)
+			endif()
+			
+			target_link_libraries(${PROJECT_ID} 
+											${DEFAULT_MAYA_LIBRARIES} 
+											${PROJECT_LINK_LIBRARIES}
+											${OS_SPECIFIC_LIBRARIES})
+			
+			# append maya specic libraries
+			foreach(LIBRARY_NAME IN LISTS PROJECT_LINK_MAYA_LIBRARIES)
+				_maya_project_id(${LIBRARY_NAME} ${MAYA_VERSION} LIBRARY_ID)
+				target_link_libraries(${PROJECT_ID} 
+										${LIBRARY_ID})
+			endforeach()
+			
+			if(PROJECT_DEFINES)
+				append_to_target_property(${PROJECT_ID} COMPILE_DEFINITIONS "${PROJECT_DEFINES}" NO)
+			endif()
+			
+			if (IS_MODULE)
+				set_target_properties(${PROJECT_ID} PROPERTIES
+														OUTPUT_NAME 
+																${PROJECT_NAME}
+														PREFIX
+																""				# make sure we don't get the 'lib' prefix on linux
+														SUFFIX
+																${PROJECT_SUFFIX}
+														LIBRARY_OUTPUT_DIRECTORY
+															${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/${MAYA_VERSION}
+														CLEAN_DIRECT_OUTPUT 1)
+				
+				if(PROJECT_WITH_TEST)
+					# Assure configuration
+					if(EXISTS ${TEST_TMRV_PATH})
+						add_test(NAME
+									${PROJECT_ID}
+								WORKING_DIRECTORY
+									${CMAKE_SOURCE_DIR}/test
+								COMMAND 
+									python ${TEST_TMRV_PATH} ${MAYA_VERSION} 
+											--mrv-mayapy batch_startup.py 
+											$<TARGET_FILE:${PROJECT_ID}>)
+						message(STATUS "Adding test for ${PROJECT_NAME} for maya ${MAYA_VERSION}")
+					else()
+						message(STATUS "Tests for ${PROJECT_ID} deactivated as tmrv path is not provided, check your configuration")
+					endif() # project with tmrv path
+				endif() # project with test
+			else()
+				set_target_properties(${PROJECT_ID} PROPERTIES
+														OUTPUT_NAME 
+																${PROJECT_NAME}
+														ARCHIVE_OUTPUT_DIRECTORY
+															${CMAKE_ARCHIVE_OUTPUT_DIRECTORY}/${MAYA_VERSION})
+			endif()# end handle module/static
+		endif()# end if devkit location exists
+		
+		
 	endforeach()# FOR EACH MAYA VERSION
 	
 	# FOR CONVENIENCE, SET THE LATEST MAYA VERSION INCLUDE PATH
